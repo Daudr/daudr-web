abf4404d861ff68f4dd41fbcb179793334a50310
Michele Da Rin <michidarin@gmail.com>
2017-10-29 23:49:20 +0100

    Aggiornata function ssr e creati script npm

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const angularUniversal = require("angular-universal-express-firebase");
 let ssrapp = angularUniversal.trigger({
-    index: __dirname + 'index.html',
-    main: __dirname + 'main.bundle',
+    index: __dirname + '/index.html',
+    main: __dirname + '/main.bundle',
     enableProdMode: true,
     cdnCacheExpiry: 2400,
     browserCacheExpiry: 1200

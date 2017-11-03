a03989ac4abd5a98ff71ef710dac16ffd3b4cb08
Michele Da Rin <michidarin@gmail.com>
2017-11-03 13:01:05 +0100

    Aggiunte competenze e tolte percentuali inutilizzate

--- src/app/components/competenze/competenze/competenze.component.ts
+++ src/app/components/competenze/competenze/competenze.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Competenza } from '../../../interfaces/interfaces';
+
 @Component({
   selector: 'app-competenze',
   templateUrl: './competenze.component.html',
   styleUrls: ['./competenze.component.css']
 })
 export class CompetenzeComponent implements OnInit {
-  competenze = [
-    { nome: 'HTML5', percentuale: 90 },
-    { nome: 'CSS3', percentuale: 80 },
-    { nome: 'Javascript', percentuale: 70 },
-    { nome: 'Angular', percentuale: 80 },
-    { nome: 'Material Design', percentuale: 75},
-    { nome: 'Java', percentuale: 60},
-    { nome: 'C', percentuale: 50 },
-    { nome: 'Assembly', percentuale: 30 }
-  ]
+  competenze: Competenza[] = [
+    { nome: 'Sviluppo Web' },
+    { nome: 'HTML5' },
+    { nome: 'CSS3' },
+    { nome: 'Javascript' },
+    { nome: 'Angular' },
+    { nome: 'Material Design' },
+    { nome: 'MongoDB' },
+    { nome: 'PHP' },
+    { nome: 'Java' },
+    { nome: 'C' },
+    { nome: 'C++' },
+    { nome: 'Assembly' },
+    { nome: 'MySQL' },
+    { nome: 'MSSQL' },
+    { nome: 'NoSQL' },
+    { nome: 'Microsoft Office' },
+    { nome: 'Project Managment' },
+    { nome: 'Problem Solving' },
+  ];
+
   constructor() { }
 
   ngOnInit() {

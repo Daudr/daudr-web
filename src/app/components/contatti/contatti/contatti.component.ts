8acf8b5d7143449fa514ab7ed76573ac655a17d5
Michele Da Rin <michidarin@gmail.com>
2017-11-08 14:02:02 +0100

    Aggiunta icona e link a ProntoPro

--- src/app/components/contatti/contatti/contatti.component.ts
+++ src/app/components/contatti/contatti/contatti.component.ts
@@ -11,6 +11,7 @@ export class ContattiComponent implements OnInit {
     { nome: 'Facebook', icon: 'assets/contatti/facebook.png', alt: 'Icona Facebook', link: 'https://www.facebook.com/micheleedarin'},
     { nome: 'LinkedIn', icon: 'assets/contatti/linkedin.png', alt: 'Icona LinkedIn', link: 'https://www.linkedin.com/in/micheleedarin/'},
     { nome: 'GitHub', icon: 'assets/contatti/github.png', alt: 'Icona GitHub', link: 'https://github.com/Daudr'},
+    { nome: 'ProntoPro', icon: 'assets/contatti/prontopro.png', alt: 'Icona ProntoPro', link: 'https://www.prontopro.it/pro/daudr-michele-da-rin'},
     { nome: 'Email', icon: 'assets/contatti/email.png', alt: 'Icona Email', link: 'mailto://michele@daudr.me', altro: 'michele@daudr.me'},
     { nome: 'Cellulare', icon: 'assets/contatti/phone.png', alt: 'Icona Facebook', link: 'tel://+393938405607', altro: '+39 3938405607'}
   ];
